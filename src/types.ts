77b4ab483ed02e4111f3375899ea6ab9e9c1af68
Johann Schopplich <mail@johannschopplich.com>
2024-12-11 16:47:29 +0100

    chore: add `upgradeable` license status

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ import type { Ref } from 'vue'
 export type MaybeRef<T = any> = T | Ref<T>
 
 export type LicenseKey = string
-export type LicenseStatus = 'active' | 'inactive' | 'invalid' | 'incompatible'
+export type LicenseStatus = 'active' | 'inactive' | 'invalid' | 'incompatible' | 'upgradeable'
 
 export interface License {
   key: LicenseKey

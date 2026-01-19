29182f9bafc5476f4acd6bd1376e69f4b8db3761
Johann Schopplich <mail@johannschopplich.com>
2026-01-19 10:52:03 +0100

    refactor: sort i18n messages

--- src/constants.ts
+++ src/constants.ts
@@ -4,43 +4,42 @@ export type Messages = Record<string, Message>
 
 export const INTEGRITY_ERROR = 'The activation buttons appear to be hidden. Please purchase a license.'
 
-export const I18N_MESSAGES: Record<Locale, Messages>
 /// keep-sorted
-  = {
-    en: {
-      'activate': 'Activate',
-      'buy': 'Buy a license',
-      'upgrade': 'Upgrade',
-      'notification.success': 'Plugin activated!',
-    },
-    de: {
-      'activate': 'Aktivieren',
-      'buy': 'Lizenz kaufen',
-      'upgrade': 'Upgrade',
-      'notification.success': 'Plugin aktiviert!',
-    },
-    fr: {
-      'activate': 'Activer',
-      'buy': 'Acheter une licence',
-      'upgrade': 'Upgrade',
-      'notification.success': 'Plugin activé !',
-    },
-    nl: {
-      'activate': 'Activeren',
-      'buy': 'Koop een licentie',
-      'upgrade': 'Upgrade',
-      'notification.success': 'Plugin geactiveerd!',
-    },
-    es: {
-      'activate': 'Activar',
-      'buy': 'Comprar licencia',
-      'upgrade': 'Actualizar',
-      'notification.success': '¡Plugin activado!',
-    },
-    it: {
-      'activate': 'Attiva',
-      'buy': 'Acquista licenza',
-      'upgrade': 'Aggiorna',
-      'notification.success': 'Plugin attivato!',
-    },
-  }
+export const I18N_MESSAGES: Record<Locale, Messages> = {
+  de: {
+    'activate': 'Aktivieren',
+    'buy': 'Lizenz kaufen',
+    'upgrade': 'Upgrade',
+    'notification.success': 'Plugin aktiviert!',
+  },
+  en: {
+    'activate': 'Activate',
+    'buy': 'Buy a license',
+    'upgrade': 'Upgrade',
+    'notification.success': 'Plugin activated!',
+  },
+  es: {
+    'activate': 'Activar',
+    'buy': 'Comprar licencia',
+    'upgrade': 'Actualizar',
+    'notification.success': '¡Plugin activado!',
+  },
+  fr: {
+    'activate': 'Activer',
+    'buy': 'Acheter une licence',
+    'upgrade': 'Upgrade',
+    'notification.success': 'Plugin activé !',
+  },
+  it: {
+    'activate': 'Attiva',
+    'buy': 'Acquista licenza',
+    'upgrade': 'Aggiorna',
+    'notification.success': 'Plugin attivato!',
+  },
+  nl: {
+    'activate': 'Activeren',
+    'buy': 'Koop een licentie',
+    'upgrade': 'Upgrade',
+    'notification.success': 'Plugin geactiveerd!',
+  },
+}
